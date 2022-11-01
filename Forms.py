c0e12fcaebf99736bd2b39c18d7ff02ad030dcc2
User <Oleg1Babchenok1@gmail.com>
2022-11-01 13:35:52 +0300

    added flask security

--- Forms.py
+++ Forms.py
@@ -4,7 +4,14 @@ from wtforms.validators import DataRequired,Email
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(),Email])
+    username = StringField('Username', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask,render_template,url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
-from Forms import LoginForm
-from models import db
+from flask_login import login_user
+from flask_security import Security, login_required, \
+     SQLAlchemySessionUserDatastore
+from Forms import LoginForm,RegisterForm
+from database import db_session, init_db
+from models import *
+from flask_security import Security, login_required, \
+     SQLAlchemySessionUserDatastore
 app = Flask(__name__)
+app.config['SECURITY_PASSWORD_SALT'] = 'some arbitrary super secret string'
 app.config['SECRET_KEY'] = '123aaaasdasdasd'
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+user_datastore = SQLAlchemySessionUserDatastore(db_session,
+                                                User, Role)
+security = Security(app, user_datastore)
+
 
 @app.route("/home")
 @app.route("/")
@@ -28,14 +43,29 @@ def about():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        login_user(user)
         username = form.username.data
         password = form.password.data
-        print(username,password)
         return redirect('/home')
     return render_template('login.html', title='Sign In', form=form)
 
+
+@app.before_first_request
+def create_user():
+    init_db()
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def Signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        username = form.username.data
+        user_datastore.create_user(email=email,username=username, password=password)
+        db_session.commit()
+        return redirect('/home')
+    return render_template('Signup.html', title='Sign Up', form=form)
+
 if __name__ == "__main__":
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
     app.run(debug = True)
